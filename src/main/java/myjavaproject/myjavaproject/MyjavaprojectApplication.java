9f69029bd62ac5a117c41c3fa4463064488ded5a
venkat <venkat@tcs.com>
2025-02-08 14:26:46 +0530

    maven with java project

---
+++ src/main/java/myjavaproject/myjavaproject/MyjavaprojectApplication.java
@@ -0,0 +1,13 @@
+package myjavaproject.myjavaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyjavaprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyjavaprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myjavaproject/myjavaproject/MyjavaprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package myjavaproject.myjavaproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyjavaprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
